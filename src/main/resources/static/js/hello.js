f68ade1ad49e613a1b7f35f22c5469f6a53ffef1
Havadasymen <jasonge0513@gmail.com>
2020-10-05 04:09:41 -0700

    update

--- src/main/resources/static/js/hello.js
+++
@@ -1,7 +0,0 @@
-angular.module('home', [])
-    .controller('Hello', function($scope, $http) {
-        $http.get('http://localhost:8080/greeting').
-        then(function(response) {
-            $scope.greeting = response.data;
-        });
-    });
